30e3290a9767229edd82969d8c5b765954c5aa6b
Mihail Șalari <info@mihailsalari.com>
2016-08-16 19:21:51 +0300

    Project initialized, Xcode 8 beta 6, Swift 3

---
+++ Pictures Locator/ViewController/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Pictures Locator
+//
+//  Created by Mihail Șalari on 8/16/16.
+//  Copyright © 2016 Mihail Șalari. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    // MARK: - Lyfe Cicle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
